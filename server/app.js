e98fec1c1965a51ec0d8b2c0e3f0833aa0c50c53
Artem Pokutnii <artempokutnij@gmail.com>
2016-11-15 22:43:43 +0200

    Added default CRUD

--- server/app.js
+++ server/app.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+import * as db from './utils/DataBaseUtils.js';
+
+db.setUpConnection();
 
 const app = express();
 
+app.use( bodyParser.json() );
+
 app.get('/', (req, res) => {
-  res.send('Hello Boy!');
+  db.listNotes().then(data => res.send(data));
+});
+
+app.post('/notes', (req, res) => {
+  db.createNote(req.body).then(data => res.send(data));
+});
+
+app.post('/notes/:id', (req, res) => {
+  db.deleteNote(req.params.id).then(data => res.send(data));
 });
 
 const server = app.listen(8080, () => {
